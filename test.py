293e7b41c9a6046d60b6048c6f33aea71c33a5f2
Alessandro1918 <alessandro1918@hotmail.com>
2024-02-04 21:12:01 -0300

    feat: show prediction accuracy at each plotted image

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 # Load data
-from tensorflow import keras
+# from tensorflow import keras
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-# Plot some images
-import matplotlib.pyplot as plt
 
 # Load the model and use it
 from tensorflow.keras.models import load_model
 import numpy as np
 
+# Plot some images
+import matplotlib.pyplot as plt
 
+# Consts:
 DATA_DIR = 'dataset_splited/test'
 IM_SHAPE = (200, 200)
 SEED = 931
@@ -20,7 +20,7 @@ BATCH_SIZE = 4
 #data_generator = ImageDataGenerator(rescale=1./255)
 # With data augmentation:
 data_generator = ImageDataGenerator(
-    rescale=1./255, 
+    # rescale=1./255, 
     rotation_range=20, 
     width_shift_range=0.2, 
     height_shift_range=0.2, 
@@ -42,22 +42,24 @@ data_generator = data_generator.flow_from_directory(
 classes = list(data_generator.class_indices.keys())
 print('Classes: ' + str(classes))   #Classes: ['Bulbasaur', 'Charmander', 'Pikachu', 'Squirtle']
 
-
 # Load model
 model = load_model('model.h5')
-
 
 # Plot some examples
 plt.figure(figsize=(10, 10))
 for i in range(9):
     #gera subfigures
-    batch = data_generator.next()[0]*255
-    image = batch[0].astype('uint8')
-    reshaped = image.reshape(1, IM_SHAPE[0], IM_SHAPE[1], 3)    # This block uses the model
-    prediction = model.predict(reshaped)                        # to predict the image classes
-    prediction_class = np.argmax(prediction, axis=-1)           # [0], [3], [2], ...
-    #plt.subplot(330 + 1 + i)
-    plt.subplot(330 + 1 + i).set_title(classes[prediction_class[0]])
+    batch = data_generator.next()[0]    # shape (4, 200, 200, 3), floats from 0-255
+    item = batch[0]                     # first item of the 4 elements array
+    rescaled = item / 255               # shape (200, 200, 3), floats from 0-1
+    image = item.astype('uint8')        # shape (200, 200, 3), ints from 0-255    
+    reshaped = rescaled.reshape(1, IM_SHAPE[0], IM_SHAPE[1], 3)     # (200, 200, 3) -> (1, 200, 200, 3)
+    predictions = model.predict(reshaped)[0]                        # [0, 0.1, 0.6, 0.3]
+    print(predictions)
+    prediction_class_index = np.argmax(predictions)                 # 2
+    prediction_class_name = classes[prediction_class_index]         # "Pikachu"
+    title = "{} ({:.2f}%)".format(prediction_class_name, 100 * np.max(predictions))     # "Pikachu (60%)"
+    plt.subplot(330 + 1 + i).set_title(title)
     plt.imshow(image)
 #plt.show()
 plt.savefig('plot_test_' + str(SEED) + '.png')
